e3a240f01cdc3439e893feb6b87760847bdad8f7
Mark Stickley <markstickley@theyakshack.co.uk>
2012-10-06 16:43:59 +0100

    First commit, just the js and the test

---
+++ name.js
@@ -0,0 +1,18 @@
+/**
+    @module name.js
+*/
+define(function() {
+
+    /**
+        Works like function.apply except you apply an object and the items
+          are indexed with the name of the arguments of the function to which
+          they are applied.
+        @param {object} scope The scope in which the function should be run
+        @param {object} args The arguments to be applied to the function
+        @returns {mixed} Whatever the function returns
+    */
+    Function.prototype.applyNamed = function(scope, args) {
+
+    };
+
+});
