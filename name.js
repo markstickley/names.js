1d890c34312f16617486ecd45ec7df2e6affa650
Mark Stickley <markstickley@theyakshack.co.uk>
2012-10-07 01:21:08 +0100

    Basic functionality plus a couple of stretch goals:
    - Added submodules jasmine & require
    - Set up test page with jasmine using require to pull in deps
    - Wrote tests & code for basic functionality
    - Write tests & code for extended functionality: defaults and argument
      order / argument caching.

--- name.js
+++ name.js
@@ -3,6 +3,54 @@
 */
 define(function() {
 
+    var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m,
+        FN_ARG_SPLIT = /,/,
+        FN_ARG = /^\s*(.+?)\s*$/,
+        STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+
+    /**
+        Takes a function and returns an array containing the names of the function's
+          arguments in the correct order
+        @param {function} func The function
+        @returns {array} The names of the function's arguments
+    */
+    function getArgsFromFunction(func) {
+        var functionText, argDeclaration, declaredArgs, matches, i,
+            args = [];
+
+        if (typeof func === 'function') {
+
+            func.__nameArgs = func.__nameArgs || {};
+
+            if(!func.__nameArgs.args) {
+
+                functionText = func.toString().replace(STRIP_COMMENTS, '');
+                argDeclaration = functionText.match(FN_ARGS)[1];
+                declaredArgs = argDeclaration.split(FN_ARG_SPLIT);
+
+                for(i = 0; i < declaredArgs.length; i++) {
+
+                    matches = declaredArgs[i].match(FN_ARG);
+
+                    if(matches !== null) {
+                        args.push(matches[1]);
+                    }
+
+                }
+
+                func.__nameArgs.args = args;
+
+            }
+
+            return func.__nameArgs.args;
+
+        }
+        else {
+            throw new Error('getArgsFromFunction: func must be a function');
+        }
+
+    }
+
     /**
         Works like function.apply except you apply an object and the items
           are indexed with the name of the arguments of the function to which
@@ -11,7 +59,26 @@ define(function() {
         @param {object} args The arguments to be applied to the function
         @returns {mixed} Whatever the function returns
     */
-    Function.prototype.applyNamed = function(scope, args) {
+    Function.prototype.applyNamed = function(scope, namedArgs) {
+
+        var i,
+            appliedArgs = [],
+            functionArgs = getArgsFromFunction(this);
+
+        for(i = 0; i < functionArgs.length; i++) {
+            if(namedArgs.hasOwnProperty(functionArgs[i])) {
+                appliedArgs.push(namedArgs[functionArgs[i]]);
+            }
+            else if(this.__nameArgs && this.__nameArgs.defaults &&
+              this.__nameArgs.defaults.hasOwnProperty(functionArgs[i])) {
+                appliedArgs.push(this.__nameArgs.defaults[functionArgs[i]]);
+            }
+            else {
+                appliedArgs.push(null);
+            }
+        }
+
+        return this.apply(scope, appliedArgs);
 
     };
 
---
+++ tests/name.js
@@ -0,0 +1,149 @@
+define(['name'], function(name) {
+
+    describe("Name.js", function() {
+
+        it("adds 'applyNamed' to Function's prototype", function() {
+            var test = function() {};
+            expect(typeof test.applyNamed).toBe('function');
+        });
+
+    });
+
+    describe("Function#applyNamed", function() {
+
+        function testScope() {
+            return this;
+        }
+
+        function testArgs(foo, bar, baz) {
+            return {
+                foo: foo,
+                bar: bar,
+                baz: baz
+            };
+        }
+
+        function testNameArgsArgs(foo, bar, baz) {
+            return {
+                foo: foo,
+                bar: bar,
+                baz: baz
+            };
+        }
+        testNameArgsArgs.__nameArgs = {
+            args: ['bar', 'foo', 'baz']
+        };
+
+        function testNameArgsDefaults(foo, bar, baz, bat) {
+            return {
+                foo: foo,
+                bar: bar,
+                baz: baz,
+                bat: bat
+            };
+        }
+        testNameArgsDefaults.__nameArgs = {
+            defaults: {
+                foo: 'ooze',
+                baz: 'banjo'
+            }
+        };
+
+        it("uses the first argument as the scope if it is an object",
+          function() {
+
+            var returnedScope,
+                scope = {
+                    pudding: 'proof'
+                };
+
+            returnedScope = testScope.applyNamed(scope, {test:0});
+
+            expect(returnedScope.pudding).toBe('proof');
+
+          });
+
+        it("uses Window as the scope if the scope is not specified",
+          function() {
+
+            var returnedScope = testScope.applyNamed(null, {test:0});
+
+            expect(returnedScope).toBe(window);
+
+          });
+
+        it("applies the arguments to the function in the order specified in the " +
+          "function definition, irrespective of the order defined in the arguments " +
+          "object", function() {
+            
+            // test for some different argument positions
+            returnedArgs = testArgs.applyNamed(null, {
+                foo: '1test',
+                baz: '2test',
+                bar: '3test'
+            });
+            expect(returnedArgs.foo).toBe('1test');
+            expect(returnedArgs.bar).toBe('3test');
+            expect(returnedArgs.baz).toBe('2test');
+
+            // test for completely different argument positions
+            var returnedArgs = testArgs.applyNamed(null, {
+                baz: 'test1',
+                foo: 'test2',
+                bar: 'test3'
+            });
+            expect(returnedArgs.foo).toBe('test2');
+            expect(returnedArgs.bar).toBe('test3');
+            expect(returnedArgs.baz).toBe('test1');
+
+          });
+
+        it("applies null to any missing arguments and applies the remaining arguments " +
+          " to the correct argument as specified in the function definition",
+          function() {
+
+            var returnedArgs = testArgs.applyNamed(null, {
+                baz: 'test1',
+                foo: 'test2'
+            });
+            expect(returnedArgs.foo).toBe('test2');
+            expect(returnedArgs.bar).toBe(null);
+            expect(returnedArgs.baz).toBe('test1');
+
+        });
+
+        it("uses static property '__nameArgs.args' to define argument order and " +
+              "override automatic detection", function() {
+
+            var returnedArgs = testNameArgsArgs.applyNamed(null, {
+                foo: 'test1',
+                bar: 'test2',
+                baz: 'test3'
+            });
+            expect(returnedArgs.foo).toBe('test2'); // because it's switched in __nameArgs.args
+            expect(returnedArgs.bar).toBe('test1');
+            expect(returnedArgs.baz).toBe('test3');
+
+        });
+
+        it("uses static property '__nameArgs.defaults' to provide default values " +
+              " to missing arguments", function() {
+
+            var returnedArgs = testNameArgsDefaults.applyNamed(null, {
+                foo: 'flibble',
+                bar: 'bonce'
+            });
+            expect(returnedArgs.foo).toBe('flibble'); // default + defined
+            expect(returnedArgs.bar).toBe('bonce'); // no default + defined
+            expect(returnedArgs.baz).toBe('banjo'); // default + undefined
+            expect(returnedArgs.bat).toBe(null); // no default + undefined
+
+        });
+
+        xit("uses static property '__nameArgs.type' to type-check arguments", function() { });
+
+        xit("uses static property '__nameArgs.validate' to validate arguments", function() { });
+
+    });
+
+});
